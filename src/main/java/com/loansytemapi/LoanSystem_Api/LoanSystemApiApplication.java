21b5a41b16847ae02814bf6ea8d54523047f07f9
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2025-03-25 07:39:07 -0500

    Commit inicial

---
+++ src/main/java/com/loansytemapi/LoanSystem_Api/LoanSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.loansytemapi.LoanSystem_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanSystemApiApplication.class, args);
+	}
+
+}
