472b42f10717e84d94daeebff6166608c5727394
alej0nt666 <alej0nt666@gmail.com>
2025-05-06 17:08:12 -0500

    Añadida la conexion a db de incomes usando jakarta persistence

--- src/main/java/com/loansytemapi/LoanSystem_Api/service/imp/IIncomeService.java
+++ src/main/java/com/loansytemapi/LoanSystem_Api/service/imp/IIncomeService.java
@@ -1,18 +1,24 @@
 package com.loansytemapi.LoanSystem_Api.service.imp;
 
-import com.loansytemapi.LoanSystem_Api.exception.IncompleteDataException;
 import com.loansytemapi.LoanSystem_Api.exception.InvalidAmmountException;
 import com.loansytemapi.LoanSystem_Api.exception.InvalidTextLengthException;
 import com.loansytemapi.LoanSystem_Api.exception.NotFoundException;
 import com.loansytemapi.LoanSystem_Api.model.Income;
-
 import java.util.List;
 
 public interface IIncomeService {
-    Income save(Income income) throws InvalidTextLengthException, InvalidAmmountException, IncompleteDataException;
-    void remove(String id) throws NotFoundException;
-    Income update(Income income) throws NotFoundException;
-    List<Income> getAll();
-    Income getByid(String id) throws NotFoundException;
-    public List<Income> getByFilters(String incomeType, Double minimumIncome, Double maximumIncome, Double incomeAmmount, String dateFilter) throws NotFoundException;
+
+    Income saveIncome(Income income) throws InvalidTextLengthException, InvalidAmmountException;
+
+    List<Income> getAllIncomes();
+
+    Income getIncomeById(Integer id) throws NotFoundException;
+
+    void deleteIncome(Integer id) throws NotFoundException;
+
+    Income updateIncome(Income income) throws NotFoundException;
+
+   List<Income> getByUserId(Integer userId) throws NotFoundException;
+
+    List<Income> getByFilter(String incomeType, Integer min, Integer max, Integer exactAmount, String dateFilter);
 }
