d0b567d9f3a7250b84555c1bb24b045a9969987c
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2025-05-06 07:46:01 -0500

    Implementando Jpa para la api de users

--- src/main/java/com/loansytemapi/LoanSystem_Api/service/imp/IUserService.java
+++ src/main/java/com/loansytemapi/LoanSystem_Api/service/imp/IUserService.java
@@ -7,7 +7,6 @@ import com.loansytemapi.LoanSystem_Api.model.User;
 import java.util.List;
 
 public interface IUserService {
-
     User saveUser(User user) throws InvalidUsernameException;
     void removeUser(String id)throws NotFoundException;
     User updateUser(User user) throws NotFoundException;
